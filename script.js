5c1db5e4b1f0c980a57c7aac41e77a4ea719488b
Javrod80 <javrod80@gmail.com>
2024-11-07 23:59:53 +0100

    7/11

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 // https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple
 
+let response = [];
+let currentQuestionIndex;
 
-function questions(){
-    fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple')
-    .then(res => res.json())
-    .then((json) =>  {
-        let response = json.results;
-        console.log(response);
-      
+function questions() {
+  fetch(
+    "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple"
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      response = json.results;
+      console.log(response);
     });
-
-
 }
 
 function makeVisual() {
+  let title = document.createElement("h1");
+  let text = document.createTextNode("Quiz Halloween: Category 'COMPUTERS'");
+  title.appendChild(text);
 
-    let title = document.createElement("h1");
-    let titulo = document.createTextNode('Quiz Halloween: Categoy "COMPUTER"');
-    let divGeneralContainer = document.createElement("div");
-	let divQuestions = document.createElement("div");
-	let divAnswers = document.createElement("div");
-    let answer = document.createElement("p");
+  let divGeneralContainer = document.createElement("div");
+  let divQuestions = document.createElement("div");
+  let divAnswers = document.createElement("div");
 
+  let showQuestionButton = document.createElement("button");
+  showQuestionButton.textContent = "Mostrar pregunta";
 
-    titulo.appendChild(title);
-    divQuestions.appendChild(divGeneralContainer);
-    divAnswers.appendChild(divGeneralContainer);
-    answer.appendChild(divAnswers);
-	
-    
+  showQuestionButton.addEventListener("click", questions);
+  divQuestions.appendChild(showQuestionButton);
 
+  divGeneralContainer.appendChild(title);
+  divGeneralContainer.appendChild(divQuestions);
+  divGeneralContainer.appendChild(divAnswers);
 
+  let optionParagraph = document.createElement("p");
+  //let optionInput = document.createElement("input");
 
+  divAnswers.appendChild(optionParagraph);
+  //divAnswers.appendChild(optionInput);
 
-
-    document.body.appendChild(divGeneralContainer); 
-
+  document.body.appendChild(divGeneralContainer);
 }
+makeVisual();
